3983cfa809590d58ad00c2a9c1b00721a814c288
Jesus <Jemiranda@utb.edu.co>
2024-10-27 16:30:26 -0500

    avance domingo funciones

--- RobotSoccersHMI/paquetes/oculus.py
+++ RobotSoccersHMI/paquetes/oculus.py
@@ -22,6 +22,9 @@ class Oculus(QThread):
     num_dispositivo = num_dispositivo
     Imageupdate = pyqtSignal(QImage)
 
+    area_local = (20, 20, 300, 320)  # (x_min, y_min, x_max, y_max)
+    area_rival = (301, 20, 600, 320)  # (x_min, y_min, x_max, y_max)
+
     # Función para detectar el color rojo en la imagen
     def detectar_color_rojo(self, frame):
         # Convertir la imagen al espacio de color HSV
@@ -47,6 +50,17 @@ class Oculus(QThread):
         
         return mask, res
     
+    def Posicion_pelota(self, cx, cy):
+        # Verificar si la pelota está en el área local
+        if (self.area_local[0] <= cx <= self.area_local[2]) and (self.area_local[1] <= cy <= self.area_local[3]):
+            return True  # Está en el área local
+        
+        # Verificar si la pelota está en el área rival
+        if (self.area_rival[0] <= cx <= self.area_rival[2]) and (self.area_rival[1] <= cy <= self.area_rival[3]):
+            return False  # Está en el área rival
+        
+        return None  # No está en ninguna de las áreas
+    
     # Dimensiones de las porterías dentro del ROI
     porteria_ancho = 50  # Ajusta según el tamaño deseado de la portería
     porteria_alto = int(area_alto * 0.2)  # Altura de la portería
@@ -66,9 +80,9 @@ class Oculus(QThread):
         cv2.rectangle(area_interes, (porteria_izquierda_x, porteria_izquierda_y),
                       (porteria_izquierda_x + self.porteria_ancho, porteria_izquierda_y + self.porteria_alto), (0, 255, 0), 2)
         
-        # Dibujar el cuadro derecho en rojo
+        # Dibujar el cuadro derecho en azullll
         cv2.rectangle(area_interes, (porteria_derecha_x, porteria_derecha_y),
-                      (porteria_derecha_x + self.porteria_ancho, porteria_derecha_y + self.porteria_alto), (0, 0, 255), 2)
+                      (porteria_derecha_x + self.porteria_ancho, porteria_derecha_y + self.porteria_alto), (255, 0, 0), 2)
 
         # Calcular los centros de cada portería
         centro_izquierda = (porteria_izquierda_x + self.porteria_ancho // 2, porteria_izquierda_y + self.porteria_alto // 2)
@@ -80,9 +94,9 @@ class Oculus(QThread):
 
         # Determinar cuál cuadro está más cerca
         if distancia_izquierda < distancia_derecha:
-            return "izquierda"
+            return "izquierda" #local
         else:
-            return "derecha"
+            return "derecha" # rival
 
     
 
@@ -115,6 +129,8 @@ class Oculus(QThread):
                 # Detectar marcadores ArUco
                 corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
                 
+                # Recortar el área de interés
+                area_interes = frame[self.area_inicio_y:self.area_inicio_y+self.area_alto, self.area_inicio_x:self.area_inicio_x+self.area_ancho]
                 # Dibujar los marcadores detectados
                 if ids is not None:
                     aruco.drawDetectedMarkers(frame, corners, ids)
@@ -130,8 +146,11 @@ class Oculus(QThread):
                         # Mostrar las coordenadas del marcador ArUco
                         cv2.putText(frame, f'({cx},{cy})', (cx + 5, cy - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-                        # Recortar el área de interés
-                area_interes = frame[self.area_inicio_y:self.area_inicio_y+self.area_alto, self.area_inicio_x:self.area_inicio_x+self.area_ancho]
+                        # Llamar a la función de proximidad y obtener el cuadro más cercano
+                        cuadro_cercano = self.dibujar_porterias_y_determinar_proximidad(area_interes, (cx, cy))
+                        cv2.putText(frame, f'Mas cerca del cuadro {cuadro_cercano}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 0), 2)
+                        print (f"el jugador esta en: {cuadro_cercano}")
+
                 
                 # Detectar el color rojo en el área de interés
                 mask, res = self.detectar_color_rojo(area_interes)
@@ -149,14 +168,17 @@ class Oculus(QThread):
                     if M["m00"] != 0:
                         cx = int(M["m10"] / M["m00"])
                         cy = int(M["m01"] / M["m00"])
+
+                        # Llamar a la función Posicion_pelota para determinar la ubicación de la pelota
+                        posicion = self.Posicion_pelota(cx, cy)
+                        if posicion is not None:
+                            estado_area = "Local" if posicion else "Rival" # if po
+                            print(f"La pelota está en el área: {estado_area}")
+
                         # Dibujar un círculo en el centro del objeto rojo
                         cv2.circle(area_interes, (cx, cy), 5, (0, 0, 0), -1)
                         # Mostrar las coordenadas en la ventana
                         cv2.putText(frame, f'({cx},{cy})', (self.area_inicio_x + cx, self.area_inicio_y + cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                    
-                        # Llamar a la función de proximidad y obtener el cuadro más cercano
-                        cuadro_cercano = self.dibujar_porterias_y_determinar_proximidad(area_interes, (cx, cy))
-                        cv2.putText(frame, f'Mas cerca del cuadro {cuadro_cercano}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 0), 2)
                 
                 # Dibujar el plano cartesiano en el área de interés
                 cv2.line(frame, (self.area_inicio_x, self.area_inicio_y + self.area_alto // 2), (self.area_inicio_x + self.area_ancho, self.area_inicio_y + self.area_alto // 2), (255, 255, 255), 2)  # Eje X
@@ -179,83 +201,3 @@ class Oculus(QThread):
     def stop(self):
         self.hilo_corriendo = False
         self.quit()
-
-    """----------------------------------------------------------"""
-   
-"""
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        
-        # Convertir la imagen a escala de grises
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-        # Detectar marcadores ArUco
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-        
-        # Dibujar los marcadores detectados
-        if ids is not None:
-            aruco.drawDetectedMarkers(frame, corners, ids)
-            
-            for marker_corners in corners:
-                # Calcular el centro del marcador
-                center = np.mean(marker_corners[0], axis=0)
-                cx, cy = int(center[0]), int(center[1])
-                
-                # Dibujar un círculo en el centro del marcador
-                cv2.circle(frame, (cx, cy), 5, (0, 255, 0), -1)
-                
-                # Mostrar las coordenadas del marcador ArUco
-                cv2.putText(frame, f'({cx},{cy})', (cx + 5, cy - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        
-        # Recortar el área de interés
-        area_interes = frame[area_inicio_y:area_inicio_y+area_alto, area_inicio_x:area_inicio_x+area_ancho]
-        
-        # Detectar el color rojo en el área de interés
-        mask, res = detectar_color_rojo(area_interes)
-        
-        # Encontrar los contornos de los objetos rojos
-        contornos, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        
-        for contorno in contornos:
-            # Ignorar pequeños contornos
-            if cv2.contourArea(contorno) < 500:
-                continue
-            
-            # Obtener las coordenadas del centro del objeto rojo
-            M = cv2.moments(contorno)
-            if M["m00"] != 0:
-                cx = int(M["m10"] / M["m00"])
-                cy = int(M["m01"] / M["m00"])
-                
-                # Dibujar un círculo en el centro del objeto rojo
-                cv2.circle(area_interes, (cx, cy), 5, (0, 0, 0), -1)
-                
-                # Mostrar las coordenadas en la ventana
-                cv2.putText(frame, f'({cx},{cy})', (area_inicio_x + cx, area_inicio_y + cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-        
-        # Dibujar el plano cartesiano en el área de interés
-        cv2.line(frame, (area_inicio_x, area_inicio_y + area_alto // 2), (area_inicio_x + area_ancho, area_inicio_y + area_alto // 2), (255, 255, 255), 2)  # Eje X
-        cv2.line(frame, (area_inicio_x + area_ancho // 2, area_inicio_y), (area_inicio_x + area_ancho // 2, area_inicio_y + area_alto), (255, 255, 255), 2)  # Eje Y
-        
-        # Dibujar el contorno de la ROI
-        cv2.rectangle(frame, (area_inicio_x, area_inicio_y), (area_inicio_x + area_ancho, area_inicio_y + area_alto), (255, 0, 0), 2)  # Cuadro azul
-        
-
-
-
-
-        # Mostrar la imagen
-        cv2.imshow('Deteccion de Color Rojo y ArUco', frame)
-        
-        # Salir al presionar 'f'
-        if cv2.waitKey(1) & 0xFF == ord('f'):
-            break
-            
-    """
-
-    # Liberar la cámara y cerrar las ventanas
-
-    #cap.release()
-    #cv2.destroyAllWindows()
