d53849e676a13b57b13ae3264844daa93cd3acd0
Jesus <Jemiranda@utb.edu.co>
2024-11-03 18:34:15 -0500

    evaluaciones casi terminadas

--- RobotSoccersHMI/paquetes/oculus.py
+++ RobotSoccersHMI/paquetes/oculus.py
@@ -3,8 +3,9 @@ import numpy as np
 import cv2.aruco as aruco
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+import math
 
-#Valores
+#Valores iniciales del ROI
 exc = 75 #pixeles excedentes
 init_x = 0
 init_y = 0 + exc
@@ -22,6 +23,11 @@ class Oculus(QThread):
     num_dispositivo = num_dispositivo
     Imageupdate = pyqtSignal(QImage)
 
+    # Dimensiones de las porterías dentro del ROI
+    porteria_ancho = 50  # Ajusta según el tamaño deseado de la portería
+    porteria_alto = int(area_alto * 0.2)  # Altura de la portería
+
+    #zona de poseción pelota
     area_local = (20, 20, 300, 320)  # (x_min, y_min, x_max, y_max)
     area_rival = (301, 20, 600, 320)  # (x_min, y_min, x_max, y_max)
 
@@ -60,12 +66,8 @@ class Oculus(QThread):
             return False  # Está en el área rival
         
         return None  # No está en ninguna de las áreas
-    
-    # Dimensiones de las porterías dentro del ROI
-    porteria_ancho = 50  # Ajusta según el tamaño deseado de la portería
-    porteria_alto = int(area_alto * 0.2)  # Altura de la portería
 
-    def dibujar_porterias_y_determinar_proximidad(self, area_interes, ids, centros):
+    def mas_cerca_arco_local(self, area_interes, ids, centros):
         """
         Dibuja los cuadros izquierdo y derecho en el área de interés y determina
         cuál marcador está más cerca de cada portería.
@@ -112,8 +114,35 @@ class Oculus(QThread):
 
         # Devolver el marcador más cercano a cada portería y sus distancias
         return id_mas_cercano_izquierda, distancia_min_izquierda, id_mas_cercano_derecha, distancia_min_derecha
+        
+         
+         # Función para calcular la distancia entre dos puntos
+       
+    def comparar_distancia_arucos(self, ids, centros, id1, id2, pelota_pos):
+    # Verificar que ambos ID estén en la lista de marcadores detectados
+        pos_id1 = None
+        pos_id2 = None
 
-    
+        for i, id_ in enumerate(ids):
+            if id_[0] == id1:
+                pos_id1 = centros[i]
+            elif id_[0] == id2:
+                pos_id2 = centros[i]
+
+        # Si alguno de los IDs no fue encontrado, devolver None
+        if pos_id1 is None or pos_id2 is None:
+            return None, None  # No se pudo realizar la comparación
+
+        # Calcular la distancia desde cada ID al objeto rojo
+        distancia_id1 = np.linalg.norm(np.array(pos_id1) - np.array(pelota_pos))
+        distancia_id2 = np.linalg.norm(np.array(pos_id2) - np.array(pelota_pos))
+
+         # Determinar cuál ID está más cerca del objeto rojo
+        if distancia_id1 < distancia_id2:
+            return id1, distancia_id1
+        else:
+            return id2, distancia_id2
+
 
     def run(self):
         self.hilo_corriendo = True
@@ -131,12 +160,6 @@ class Oculus(QThread):
         while self.hilo_corriendo:
             ret, frame = cap.read()
             if ret:
-                """Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                #flip = cv2.flip(Image, 1)
-                flip = Image
-                convertir_QT = QImage(flip.data, flip.shape[1], flip.shape[0], QImage.Format_RGB888)
-                pic = convertir_QT.scaled(720, 480, Qt.KeepAspectRatio)
-                self.Imageupdate.emit(pic)"""
 
                 # Convertir la imagen a escala de grises
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -146,33 +169,43 @@ class Oculus(QThread):
                 
                 # Recortar el área de interés
                 area_interes = frame[self.area_inicio_y:self.area_inicio_y+self.area_alto, self.area_inicio_x:self.area_inicio_x+self.area_ancho]
+                
+                #Inicializar centros como lista
+                centros =[] #array para la funcion
+
                 # Dibujar los marcadores detectados
                 if ids is not None:
                     aruco.drawDetectedMarkers(frame, corners, ids)
-                    centros =[] #array para la funcion
+                    
                     for marker_corners in corners:
                         # Calcular el centro del marcador
                         center = np.mean(marker_corners[0], axis=0)
-                        cx, cy = int(center[0]), int(center[1])
-                        centros.append((cx, cy)) #array para la funcion
-                        
+
+                        px, py = int(center[0]), int(center[1]) #cambiar nombre
+                        centros.append((px, py)) #array para la funcion
+
+                        #Calcular el vector  de direccion frontal (El frente se asume entre las esquinas 0 y 3)
+                        front_direccion =  marker_corners[0][0] - marker_corners[0][3]
+                        front_direccion = front_direccion/np.linalg.norm(front_direccion) # normalizar el vector
+
+                        front_point = (int(px + front_direccion[0]*30),int(py + front_direccion[1]*30))
+                        cv2.line(frame,(px,py),front_point, (0,255,255),2) # Dibujar la linea para indicar el frente
+
                         # Dibujar un círculo en el centro del marcador
-                        cv2.circle(frame, (cx, cy), 5, (0, 255, 0), -1)
+                        cv2.circle(frame, (px, py), 5, (0, 255, 0), -1)
                         
                         # Mostrar las coordenadas del marcador ArUco
-                        cv2.putText(frame, f'({cx},{cy})', (cx + 5, cy - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                        cv2.putText(frame, f'({px},{py})', (px + 5, py - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-                        """ # Llamar a la función de proximidad y obtener el cuadro más cercano
-                        cuadro_cercano = self.dibujar_porterias_y_determinar_proximidad(area_interes, (cx, cy))
-                        cv2.putText(frame, f'Mas cerca del cuadro {cuadro_cercano}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 0), 2)
-                        print (f"el jugador esta en: {cuadro_cercano}") """
+                        # Si hay pelota detectada, calcular el ángulo con el frente del marcador
+                      
                      # Llamar a la función de proximidad y obtener los marcadores más cercanos a cada portería
-                    id_izquierda, dist_izquierda, id_derecha, dist_derecha = self.dibujar_porterias_y_determinar_proximidad(area_interes, ids, centros)
+                    id_izquierda, dist_izquierda, id_derecha, dist_derecha = self.mas_cerca_arco_local(area_interes, ids, centros)
                     
                     # Mostrar el ID y la distancia de los marcadores más cercanos en pantalla
                     cv2.putText(frame, f'Más cercano a izquierda: ID {id_izquierda} ({dist_izquierda:.2f} px)', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
                     cv2.putText(frame, f'Más cercano a derecha: ID {id_derecha} ({dist_derecha:.2f} px)', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-                    print("id izq=",id_izquierda, dist_izquierda, "id dere=", id_derecha, dist_derecha)    
+                    #print("id izq=",id_izquierda, dist_izquierda, "id dere=", id_derecha, dist_derecha)    
 
 
 
@@ -183,6 +216,8 @@ class Oculus(QThread):
                 # Encontrar los contornos de los objetos rojos
                 contornos, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+                pelota_pos= None #array posición pelota ###############
+                
                 for contorno in contornos:
                     # Ignorar pequeños contornos
                     if cv2.contourArea(contorno) < 500:
@@ -193,18 +228,29 @@ class Oculus(QThread):
                     if M["m00"] != 0:
                         cx = int(M["m10"] / M["m00"])
                         cy = int(M["m01"] / M["m00"])
+                        
+                        pelota_pos=(cx,cy) #posicion de la pelota ###################
 
                         # Llamar a la función Posicion_pelota para determinar la ubicación de la pelota
                         posicion = self.Posicion_pelota(cx, cy)
                         if posicion is not None:
                             estado_area = "Local" if posicion else "Rival" # if po
-                            print(f"La pelota está en el área: {estado_area}")
+                            ###############print(f"La pelota está en el área: {estado_area}")
 
                         # Dibujar un círculo en el centro del objeto rojo
                         cv2.circle(area_interes, (cx, cy), 5, (0, 0, 0), -1)
                         # Mostrar las coordenadas en la ventana
                         cv2.putText(frame, f'({cx},{cy})', (self.area_inicio_x + cx, self.area_inicio_y + cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                
+                        
+                        if centros:
+                            # Comparar distancia entre IDs específicos y el objeto rojo
+                            id_mas_cercano, distancia = self.comparar_distancia_arucos(ids, centros, 1, 0, pelota_pos)
+                    
+                            # Mostrar en pantalla cuál ID está más cerca del objeto rojo
+                            if id_mas_cercano is not None:
+                                cv2.putText(frame, f'ID {id_mas_cercano} está más cerca del objeto rojo ({distancia:.2f}px)', (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
+                                print(f"Id mas cercano {id_mas_cercano}")
+
                 # Dibujar el plano cartesiano en el área de interés
                 cv2.line(frame, (self.area_inicio_x, self.area_inicio_y + self.area_alto // 2), (self.area_inicio_x + self.area_ancho, self.area_inicio_y + self.area_alto // 2), (255, 255, 255), 2)  # Eje X
                 cv2.line(frame, (self.area_inicio_x + self.area_ancho // 2, self.area_inicio_y), (self.area_inicio_x + self.area_ancho // 2, self.area_inicio_y + self.area_alto), (255, 255, 255), 2)  # Eje Y
