da0ca3bdcd4586f522915aa53b3aa23e8743b41e
iCruzDaniel <sp_dicruz044@hotmail.com>
2024-11-03 17:54:45 -0500

    Creación del arbol de decisiones

---
+++ RobotSoccersHMI/paquetes/tree.py
@@ -0,0 +1,36 @@
+from oculus import * 
+
+class Tree:
+    def __init__(self):
+
+        self.arbol  = {
+            "vvvffv": "horario|baja|0",
+            "vvvfff": "antihorario|baja|0",
+
+            "vvffv": "horario|baja|0",
+            "vvfff": "antihorario|baja|0",
+
+            "vfvffv": "horario|baja|0",
+            "vfvfff": "antihorario|baja|0",
+
+            "vfffv": "horario|baja|0",
+            "vffff": "antihorario|baja|0",
+
+            "fvvffv": "horario|baja|0",
+            "fvvffv": "antihorario|baja|0",
+
+            "fvffv": "horario|baja|0",
+            "fvfff": "antihorario|baja|0",
+
+            "ffvffv": "horario|baja|0",
+            "ffvfff": "antihorario|baja|0",
+
+            "ffffv": "horario|baja|0",
+            "fffff": "antihorario|baja|0"
+        }
+        
+    def eval(self):
+        index = ""
+        
+        
+        return self.arbol[index]
